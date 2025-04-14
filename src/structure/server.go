0e518596bc04c4e304ede1032eeeaf03d20ba4f2
Bojan <bojanpopovred@gmail.com>
2025-04-14 15:10:41 +0200

    Renamed the file and extended the structure with one more parametar

--- src/structure/server.go
+++
@@ -1,8 +0,0 @@
-package structure
-
-type ServerConfig struct {
-	Port string `json:"port"`
-	SessionActiveInterval int `json:"session_active_interval_hours"`
-	RateLimitMaxToken int `json:"rate_limit_max_sessions"`
-	RateLimitRefillDuration int `json:"rate_limit_refill_duration_secods"`
-}
